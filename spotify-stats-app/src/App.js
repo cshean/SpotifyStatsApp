dc5f24e07e5a427f45847b64841548ece89481d7
cshean <cartershean2@gmail.com>
2022-07-19 13:14:39 -0400

    Initial commit

---
+++ spotify-stats-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">
+        primary
+      </button>
+    </div>
+  );
+}
+
+export default App;
